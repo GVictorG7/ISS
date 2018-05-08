c1e39205f72bdc0e72cf9349952e6755b9f0009e
ot3107487 <bogdan_danut_11@yahoo.com>
2018-05-08 16:17:40 +0300

    fixed error from console. BrowserAnimationModule added

--- ISS_FRONTEND/blood-donation-app/src/app/app.module.ts
+++ ISS_FRONTEND/blood-donation-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {HomeComponent} from './home/home.component';
 import {CoreModule} from "./core/core.module";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import {CoreModule} from "./core/core.module";
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     CoreModule,
     AppRoutingModule
   ],
