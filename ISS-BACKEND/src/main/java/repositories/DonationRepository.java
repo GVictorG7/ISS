ec57518c27b05807c5fe58cdb06ddab3ec923749
virtkx <gradinariu.george97@gmail.com>
2018-05-19 20:49:35 +0300

    repo, service, controller for Blood

---
+++ ISS-BACKEND/src/main/java/repositories/DonationRepository.java
@@ -0,0 +1,7 @@
+package repositories;
+
+import model.Donation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DonationRepository extends JpaRepository<Donation,Long> {
+}
